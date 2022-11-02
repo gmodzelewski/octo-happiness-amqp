61accc64510fcdf03b5355241cda3b3daea2e4fa
Georg Modzelewski <georg@redhat.com>
2022-11-02 14:14:04 +0100

    working with elapsed time output

--- amqp-quickstart-processor/src/main/java/org/acme/amqp/model/Quote.java
+++ amqp-quickstart-processor/src/main/java/org/acme/amqp/model/Quote.java
@@ -4,18 +4,24 @@ import io.quarkus.runtime.annotations.RegisterForReflection;
 
 @RegisterForReflection
 public class Quote {
-
     public String id;
     public int price;
+    public long requestStartTime;
+    public long requestTimeInMs;
+    public long quoteStartTime;
 
     /**
-    * Default constructor required for Jackson serializer
-    */
-    public Quote() { }
+     * Default constructor required for Jackson serializer
+     */
+    public Quote() {
+    }
 
-    public Quote(String id, int price) {
+    public Quote(String id, int price, long requestStartTime, long requestTimeInMs, long quoteStartTime) {
         this.id = id;
         this.price = price;
+        this.requestStartTime = requestStartTime;
+        this.requestTimeInMs = requestTimeInMs;
+        this.quoteStartTime = quoteStartTime;
     }
 
     @Override
@@ -23,6 +29,9 @@ public class Quote {
         return "Quote{" +
                 "id='" + id + '\'' +
                 ", price=" + price +
+                ", requestStartTime=" + requestStartTime +
+                ", requestTimeInMs=" + requestTimeInMs +
+                ", quoteStartTime=" + quoteStartTime +
                 '}';
     }
 }
--- amqp-quickstart-processor/src/main/java/org/acme/amqp/processor/QuoteProcessor.java
+++ amqp-quickstart-processor/src/main/java/org/acme/amqp/processor/QuoteProcessor.java
@@ -1,14 +1,16 @@
 package org.acme.amqp.processor;
 
-import java.util.Random;
-
-import javax.enterprise.context.ApplicationScoped;
-
+import io.smallrye.reactive.messaging.annotations.Blocking;
+import io.vertx.core.json.JsonObject;
 import org.acme.amqp.model.Quote;
+import org.acme.amqp.model.QuoteRequest;
 import org.eclipse.microprofile.reactive.messaging.Incoming;
 import org.eclipse.microprofile.reactive.messaging.Outgoing;
 
-import io.smallrye.reactive.messaging.annotations.Blocking;
+import javax.enterprise.context.ApplicationScoped;
+import java.util.Random;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 /**
  * A bean consuming data from the "request" AMQP queue and giving out a random quote.
@@ -17,12 +19,19 @@ import io.smallrye.reactive.messaging.annotations.Blocking;
 @ApplicationScoped
 public class QuoteProcessor {
 
+    private final Logger logger = Logger.getLogger("QuoteProcessor.java");
     private final Random random = new Random();
 
     @Incoming("requests")       // <1>
     @Outgoing("quotes")         // <2>
     @Blocking                   // <3>
-    public Quote process(String quoteRequest) {
-        return new Quote(quoteRequest, random.nextInt(100));
+    public Quote process(JsonObject jsonObject) {
+        final String id = jsonObject.getString("id", "dinge");
+        final long startTime = jsonObject.getLong("start_time", 1L);
+        final QuoteRequest quoteRequest = new QuoteRequest(id, startTime);
+        final long now = System.currentTimeMillis();
+        final long quoteRequestTimeInMs = now - startTime;
+        logger.log(Level.INFO, "QuoteRequest ran " + quoteRequestTimeInMs + "ms");
+        return new Quote(quoteRequest.id, random.nextInt(100), quoteRequest.startTime, quoteRequestTimeInMs, System.currentTimeMillis());
     }
 }
